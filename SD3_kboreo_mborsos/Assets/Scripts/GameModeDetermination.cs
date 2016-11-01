6712fc69cffca9347f4700180f6fc13a7bc22a40
Kboreo <kboreo@ltu.edu>
2016-10-31 21:52:32 -0400

    Added computerMode to DataContainer, created selNumOfPlayers function

--- SD3_kboreo_mborsos/Assets/Scripts/GameModeDetermination.cs
+++ SD3_kboreo_mborsos/Assets/Scripts/GameModeDetermination.cs
@@ -1,6 +1,7 @@
 ﻿//Authors: Kodey Boreo and Marshall Borsos
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameModeDetermination : MonoBehaviour {
 
@@ -20,7 +21,10 @@ public class GameModeDetermination : MonoBehaviour {
 
 	//Function for when user clicks "Player vs Computer" button
 	public void playerVsComputerButton_Click()
-	{
+	{		
+		//Enables computer logic
+		selNumOfPlayers(1);
+
 		//Disables current group object to clear canvas 
 		GameObject.Find("Game Mode Menu").GetComponent<CanvasGroup>().alpha = 0;
 		GameObject.Find("Game Mode Menu").GetComponent<CanvasGroup>().interactable = false;
@@ -30,11 +34,15 @@ public class GameModeDetermination : MonoBehaviour {
 		GameObject.Find("Start Game Menu").GetComponent<CanvasGroup>().alpha = 1;
 		GameObject.Find("Start Game Menu").GetComponent<CanvasGroup>().interactable = true;
 		GameObject.Find("Start Game Menu").GetComponent<CanvasGroup>().blocksRaycasts = true;
+
 	}
 
 	//Function for when user clicks "Cancel" button located on the Start Game Menu
-	public void cancelButton_Click()
+	public void cancelSelection()
 	{
+		//Disables computer logic
+		selNumOfPlayers(0);
+
 		//Disables the canvas group to clear the canvas
 		GameObject.Find("Start Game Menu").GetComponent<CanvasGroup>().alpha = 0;
 		GameObject.Find("Start Game Menu").GetComponent<CanvasGroup>().interactable = false;
@@ -44,5 +52,20 @@ public class GameModeDetermination : MonoBehaviour {
 		GameObject.Find("Game Mode Menu").GetComponent<CanvasGroup>().alpha = 1;
 		GameObject.Find("Game Mode Menu").GetComponent<CanvasGroup>().interactable = true;
 		GameObject.Find("Game Mode Menu").GetComponent<CanvasGroup>().blocksRaycasts = true;
+	}
+
+	//Function that changes enables or disables computer logic (0 = computerLogic is disabled, 1 = computerLogic enabled)
+	public void selNumOfPlayers(int players)
+	{		
+		if (players == 0) 
+		{
+			//Disables computer logic
+			DataContainer.computerLogic = false;
+		}
+		else if (players == 1)
+		{
+			//Enables computer logic
+			DataContainer.computerLogic = true;
+		}
 	}
 }
